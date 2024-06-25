35f0c5f17d6ba38554d611c37b7ba03a25037469
Mansi-Kain <100782274+Mansi-Kain@users.noreply.github.com>
2024-06-25 14:31:07 +0530

    Add files via upload

---
+++ banking-app/src/main/java/net/javaguides/banking/entity/Account.java
@@ -0,0 +1,28 @@
+package net.javaguides.banking.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name="accounts")
+@Entity
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name= "account_holder_name")
+    private String accountHolderName;
+    private double balance;
+    public Account(Long id, String accountHolderName, double balance) {
+        this.id = id;
+        this.accountHolderName = accountHolderName;
+        this.balance = balance;
+    }
+
+}
